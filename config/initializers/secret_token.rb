50b56d52763a523e1b35a7b0c3f6ce1660cd6300
Eric Levine <djlevine@gmail.com>
2013-07-01 10:13:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridraffenBlog::Application.config.secret_token = '5af256f5b594f3e4c27b690400c89816c72226f13830bb94e15de28b15925905f232a1bef3c430f57283822e8365bbf0e5241320b017defdafba94ca46dffb11'
